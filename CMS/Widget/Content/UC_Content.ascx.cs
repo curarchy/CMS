d59c31caf8ca2863a9d00686d00da73eb6667192
curarchy <curarpiktchen@hotmail.com>
2013-06-27 09:05:47 +0800

    Modify

--- CMS/Widget/Content/UC_Content.ascx.cs
+++ CMS/Widget/Content/UC_Content.ascx.cs
@@ -4,6 +4,12 @@ namespace CMS.Widget.Content
 {
     public partial class UC_Content : UserControlBase
     {
+        private string articleID;
 
+        public string ArticleID
+        {
+            get;
+            set;
+        }
     }
 }
